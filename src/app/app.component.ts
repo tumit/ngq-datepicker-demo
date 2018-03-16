ca1a93768a2b1d04fbae70a0045cc1fa8d0533b9
tumit <tumit@live.com>
2018-03-16 13:40:33 +0700

    feat: use bootstrap-datepicker-year-offset

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { th_TH, en_GB } from 'ngq-datepicker';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  opts = th_TH;
+
+  toggle() {
+    this.opts = (this.opts === th_TH) ? en_GB : th_TH;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { NgqDatepickerModule, th_TH } from 'ngq-datepicker';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, NgqDatepickerModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
